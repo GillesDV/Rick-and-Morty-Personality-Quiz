8df427bee12f0b9e865d596231e80f9931875e65
GillesDV <gilles.devylder@hotmail.com>
2018-05-23 20:11:50 +0200

    add the basis of the ResultCalculator

--- PersonalityQuiz/PersonalityQuiz/Domain/QuizResultCalculator.cs
+++ PersonalityQuiz/PersonalityQuiz/Domain/QuizResultCalculator.cs
@@ -8,8 +8,54 @@ namespace PersonalityQuiz.Domain
 {
     public class QuizResultCalculator
     {
+        private static int _rickCounter = 0;
+        private static int _mortyCounter = 0;
+        private static int _jerryCounter = 0;
+        private static int _bethCounter = 0;
+        private static int _summerCounter = 0;
+        private static int _mrMeeseeksCounter = 0;
+        private static int _birdPersonCounter = 0;
+
+
         public static string CalculateResult(List<int> allAnswers)
         {
+            switch (allAnswers[0])
+            {
+                case 1:
+                    _rickCounter++;
+                    _mortyCounter++;
+                    _jerryCounter++;
+                    break;
+                case 2:
+                    _summerCounter++;
+                    _bethCounter++;
+                    break;
+                case 3:
+                    _mrMeeseeksCounter++;
+                    break;
+            }
+
+            switch (allAnswers[1])
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+            }
+
+            switch (allAnswers[3])
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    _mrMeeseeksCounter += 20;
+                    break;
+            }
+
 
             return null;
         }
