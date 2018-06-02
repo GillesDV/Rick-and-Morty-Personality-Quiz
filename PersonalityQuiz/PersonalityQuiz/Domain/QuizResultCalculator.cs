ab4edcd5fbe1a470176636be16c504121ab3645d
GillesDV <gilles.devylder@hotmail.com>
2018-06-02 19:10:00 +0200

    Add results class

--- PersonalityQuiz/PersonalityQuiz/Domain/QuizResultCalculator.cs
+++ PersonalityQuiz/PersonalityQuiz/Domain/QuizResultCalculator.cs
@@ -13,8 +13,8 @@ namespace PersonalityQuiz.Domain
         private static int _jerryCounter = 0;
         private static int _bethCounter = 0;
         private static int _summerCounter = 0;
+
         private static int _mrMeeseeksCounter = 0;
-        private static int _birdPersonCounter = 0;
 
 
         public static string CalculateResult(List<int> allAnswers)
@@ -34,8 +34,50 @@ namespace PersonalityQuiz.Domain
                     _mrMeeseeksCounter++;
                     break;
             }
-
             switch (allAnswers[1])
+            {
+                case 1:
+                    _jerryCounter++;
+                    _summerCounter++;
+                    break;
+                case 2:
+                    _summerCounter++;
+                    _bethCounter++;
+                    break;
+                case 3:
+                    _rickCounter++;
+                    _mortyCounter++;
+                    break;
+            }
+            switch (allAnswers[2])
+            {
+                case 1:
+
+                    break;
+                case 2:
+                    _rickCounter++;
+                    break;
+                case 3:
+                    _rickCounter++;
+                    _mortyCounter++;
+                    _bethCounter++;
+                    _jerryCounter++;
+                    break;
+            }
+            switch (allAnswers[3])
+            {
+                case 1:
+                    _summerCounter++;
+                    _bethCounter++;
+                    _jerryCounter++;
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    _mrMeeseeksCounter += 20;
+                    break;
+            }
+            switch (allAnswers[4])
             {
                 case 1:
                     break;
@@ -45,14 +87,50 @@ namespace PersonalityQuiz.Domain
                     break;
             }
 
-            switch (allAnswers[3])
+
+            switch (allAnswers[5])
             {
                 case 1:
                     break;
                 case 2:
                     break;
                 case 3:
-                    _mrMeeseeksCounter += 20;
+                    break;
+            }
+            switch (allAnswers[6])
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+            }
+            switch (allAnswers[7])
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+            }
+            switch (allAnswers[8])
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+            }
+            switch (allAnswers[9])
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
                     break;
             }
 
---
+++ PersonalityQuiz/PersonalityQuiz/Domain/Result.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalityQuiz.Domain
+{
+    public enum Result
+    {
+        Rick,
+        Morty,
+        Summer,
+        Betch,
+        Jerry,
+        MrMeeSeeks
+    }
+}
