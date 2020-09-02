e2fbdfe0d4ca1592431a94265b787a833395caba
GillesDV <gilles.devylder@hotmail.com>
2020-09-02 13:49:51 +0200

    add a faulty unit test to test the automatic build

---
+++ PersonalityQuiz/test/PersonalityQuiz.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PersonalityQuiz.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PersonalityQuiz.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dc9ae2d4-1be6-4566-809e-e854fab8d9bd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PersonalityQuiz/test/PersonalityQuiz.UnitTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PersonalityQuiz.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethodForAutomaticBuild()
+        {
+            bool thisVariableIsFalse = true;
+
+            Assert.AreEqual(false, thisVariableIsFalse);
+        }
+    }
+}
